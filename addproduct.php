c67d103f247f046d56ac40642beab5df13b6fcf4
gagva <goderdzi.valishvili.1@btu.edu.ge>
2023-05-02 22:46:27 +0400

    second commit

---
+++ addproduct.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>goderdzi_valishvili</title>
+    <link rel="stylesheet" href="styles/css/spage_styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="main">
+        <!--code belowe contains heading, add/del buttons and hr underline -->
+        <div class="heading">
+            <h1>Product Add</h1>
+            <button class="save-btn" type="button">Save</button>
+            <button class="cancel-btn" type="button">Cancel</button>
+        </div>
+        <hr>
+        <form id=product_form>
+            <label for="sku">SKU</label>
+            <input id="sku" name="sku" type="text" value="#sku">
+            <label for="name">Name</label>
+            <input id="name" name="name" type="text"  value="#name">
+            <label for="price">Price ($)</label>
+            <input id="price" name="price" type="text"  value="#price">
+        </form>
+        <hr>
+        <p>Scandiweb Test assignment</p>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+        integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+        integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
